e4ac6a01d0300a51aa0365bfe35972120b9c9f8b
icarante <slqughterhquse@gmail.com>
2024-02-15 09:55:14 -0800

    I UNDERSTOOD ONE (had to find it online)

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,5 +1,5 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
-// Your code here.
+let newArray = arrays.reduce((total, nums) => total.concat(nums), []);
 
-
+console.log(newArray);
 // → [1, 2, 3, 4, 5, 6]
--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,5 +1,12 @@
 // Your code here.
 
+function loop(start, until, step, action){
+    for(i=start; until; step){
+        return i;
+    }
+    action(i);
+}
+
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
