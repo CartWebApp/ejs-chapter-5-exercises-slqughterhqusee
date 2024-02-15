6d9498da6587a5a31e00c91f3339640191f4f4c0
icarante <slqughterhquse@gmail.com>
2024-02-15 10:40:13 -0800

    YYAYAYYAYAYAYAY

--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,13 +1,13 @@
 // Your code here.
 
 function loop(start, until, step, action){
-    if (until === false){
+    if (until(start) === false){
         return;
     }
 
     action(start);
 
-    
+    return(loop(step(start), until, step,action));
 }
 
 loop(3, n => n > 0, n => n - 1, console.log);
