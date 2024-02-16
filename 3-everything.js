9dbee8713468526fb77fe999982f603c58b71615
icarante <slqughterhquse@gmail.com>
2024-02-16 08:39:59 -0800

    YAYAYAYYAYAYAYAYAAYYAAAA

--- 3-everything.js
+++ 3-everything.js
@@ -1,10 +1,22 @@
 function every(array, test) {
-    // Your code here.
+  for (i = 0; i < array.length; i++) {
+    if (test(array[i])==false){
+      return false;
+    }
   }
-  
-  console.log(every([1, 3, 5], n => n < 10));
-  // → true
-  console.log(every([2, 4, 16], n => n < 10));
-  // → false
-  console.log(every([], n => n < 10));
-  // → true
+  return true;
+}
+
+function everySome(array, test){
+  return !array.some(test);
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+
+console.log(everySome([2, 4, 16], n => n < 10));
+// → false
